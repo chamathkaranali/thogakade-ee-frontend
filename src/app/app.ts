b35491a794787c59c7ee759c41931d571678c719
Chamathka Ranali <ranalichamathka@gmail.com>
2026-02-18 22:18:33 +0530

    complete frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Dashboard } from "./page/dashboard/dashboard";
+import { Login } from "./page/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
